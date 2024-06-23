8073b2b6581b6502b98e79dcdf3a6c8fe4dabdf4
Lincoln Hubbard <hubbard.lincoln@gmail.com>
2024-06-22 19:39:01 -0700

    make font size smaller for glossary terms

--- pages/4_Glossary.py
+++ pages/4_Glossary.py
@@ -6,7 +6,16 @@ st.title('Glossary of Key Terms')
 
 terms = pd.read_csv('key_terms.csv')
 
+#make font sizes smaller
+st.markdown("""
+    <style>
+    .small-font {
+        font-size:12px !important;
+    }
+    </style>
+    """, unsafe_allow_html=True)
+
 for index, row in terms.iterrows():
-    st.subheader(row['Term'])
-    st.markdown(row['Definition'], unsafe_allow_html=True)
+    st.markdown(f"<h3 class='small-font'>{row['Term']}</h3>", unsafe_allow_html=True)
+    st.markdown(f"<p class='small-font'>{row['Definition']}</p>", unsafe_allow_html=True)
 
